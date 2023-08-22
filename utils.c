d02011194cecc99ef54113969b4a65ece5ba5b5c
abdelhamid2004 <abdelhamideslam900@gmail.com>
2023-08-22 09:31:34 -0700

    acc

--- utils.c
+++ utils.c
@@ -80,7 +80,7 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
-return ((unsigned short)num);
+		return ((unsigned short)num);
 
 	return ((unsigned int)num);
 }
